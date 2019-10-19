e52c0a6cf49e321b1ea5037be0dfe1d1c68984e5
akinbowale <akinbowale.akt@gmail.com>
2019-10-19 15:36:20 +0100

    check running mode before using logger

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(logger);
+}
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
