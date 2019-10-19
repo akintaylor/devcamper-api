b94fc69494effd7806b34f4956a047a03f9fe7d5
akinbowale <akinbowale.akt@gmail.com>
2019-10-19 15:32:57 +0100

    create and use custom logger

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middleware/logger");
 
 // Route files
-const bootcamps = require('./routes/bootcamps');
+const bootcamps = require("./routes/bootcamps");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.use(logger);
+
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT;
 
