3df97ee9bd7cff25d3db49abd3e68d9764b01d3e
akinbowale <akinbowale.akt@gmail.com>
2020-05-28 10:47:16 +0100

    feat: user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,3 +23,36 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, token });
 });
+
+/**
+ * @description Login user
+ * @route POST /api/v1/auth/login
+ * @access public
+ */
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email: email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
