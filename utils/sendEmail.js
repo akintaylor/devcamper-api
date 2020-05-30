d363a7eaa5bac993ce35bf001f7ff8962bc8c8d6
akinbowale <akinbowale.akt@gmail.com>
2020-05-30 02:43:54 +0100

    feat: create email module in utils

---
+++ utils/sendEmail.js
@@ -0,0 +1,26 @@
+"use strict";
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
+};
+
+module.exports = sendEmail;
