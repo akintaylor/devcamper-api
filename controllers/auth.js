cf596025aee0458a652ff2829e82a36d68c200d4
akinbowale <akinbowale.akt@gmail.com>
2020-05-31 16:58:29 +0100

    feat: use cookie to login and logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,6 +57,22 @@ exports.login = asyncHandler(async (req, res, next) => {
 });
 
 /**
+ * @description  Log user out / clear cookie
+ * @route GET /api/v1/auth/logout
+ * @access private
+ */
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
+/**
  * @description Get current logged in user
  * @route GET /api/v1/auth/me
  * @access private
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   register,
   login,
+  logout,
   getMe,
   forgotPassword,
   resetPassword,
@@ -14,6 +15,7 @@ const router = express.Router();
 const { protect } = require("../middlewares/auth");
 
 router.post("/login", login);
+router.get("/logout", logout);
 router.post("/register", register);
 router.get("/me", protect, getMe);
 router.put("/updatedetails", protect, updateDetails);
